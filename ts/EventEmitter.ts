792dda49491214532cd307c7ee3da2c82149257f
Jesse Donat <donatj@gmail.com>
2017-06-26 16:59:55 -0500

    Adds EventEmitter

---
+++ ts/EventEmitter.ts
@@ -0,0 +1,14 @@
+type Listener<T> = (e: T) => void;
+
+class EventEmitter<T> {
+	protected listeners = new Set<Listener<T>>();
+
+	public add(callback: Listener<T>) {
+		this.listeners.add(callback);
+	}
+
+	public trigger( event: T ) {
+		this.listeners.forEach((fn) => fn(event));
+	}
+
+}
